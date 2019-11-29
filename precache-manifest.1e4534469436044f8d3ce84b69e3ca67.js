b98d775626c104101c593500593ad618c0067a71
gleb <gkebkasyak@gmail.com>
2019-11-29 19:58:54 +0300

    Updates

---
+++ precache-manifest.1e4534469436044f8d3ce84b69e3ca67.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7ded20afeefa7c455c19d11406ed480",
+    "url": "/Social-Network/index.html"
+  },
+  {
+    "revision": "ff24a8314d14ee3ba03e",
+    "url": "/Social-Network/static/css/2.cda40311.chunk.css"
+  },
+  {
+    "revision": "a2f93840cbf2f77f523f",
+    "url": "/Social-Network/static/css/3.594ee4eb.chunk.css"
+  },
+  {
+    "revision": "ef5b11d58fc0d6270e01",
+    "url": "/Social-Network/static/css/4.7d629c34.chunk.css"
+  },
+  {
+    "revision": "d5a2a77bb86a31970ce7",
+    "url": "/Social-Network/static/css/main.3c081ae7.chunk.css"
+  },
+  {
+    "revision": "ff24a8314d14ee3ba03e",
+    "url": "/Social-Network/static/js/2.f9cf9199.chunk.js"
+  },
+  {
+    "revision": "a2f93840cbf2f77f523f",
+    "url": "/Social-Network/static/js/3.e4ade7c3.chunk.js"
+  },
+  {
+    "revision": "ef5b11d58fc0d6270e01",
+    "url": "/Social-Network/static/js/4.1c6bbeb5.chunk.js"
+  },
+  {
+    "revision": "d5a2a77bb86a31970ce7",
+    "url": "/Social-Network/static/js/main.2f27ee54.chunk.js"
+  },
+  {
+    "revision": "b44603f9263735ce77a7",
+    "url": "/Social-Network/static/js/runtime-main.69aad38d.js"
+  },
+  {
+    "revision": "d2bae9842aa1cd36135712940c77530a",
+    "url": "/Social-Network/static/media/avtar.d2bae984.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Social-Network/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Social-Network/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Social-Network/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Social-Network/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Social-Network/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "46fda329a2cd1f8c89987c72ac8ee8c1",
+    "url": "/Social-Network/static/media/logo.46fda329.png"
+  },
+  {
+    "revision": "9353c0310b620a439b69adfc8b75d27f",
+    "url": "/Social-Network/static/media/wrong_icon.9353c031.png"
+  }
+]);
